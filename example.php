51e390be89d7d96b7504d5f047b5880225f1b133
Jamie Sefton <jamie.sefton@parall.ax>
2016-09-28 18:54:32 +0100

    Updated to clear out tests and include empty output folder

--- example.php
+++ example.php
@@ -2,8 +2,6 @@
 require_once "config.php";
 
 
-$staticize = Staticize::init("https://forpaws.info","http://static.app")->download();
-print_r($staticize);
-die;
+$staticize = Staticize::init("<DOMAIN>","<LOCAL_DOMAIN>")->download();
 
 ?>
